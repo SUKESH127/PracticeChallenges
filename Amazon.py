45042b0c0d2cfa6d6cec0fdf9681890bc7ecfb38
Sukesh Ram <sukeshram7245@gmail.com>
2021-01-26 17:47:34 -0700

    Update Amazon.py

--- Amazon.py
+++ Amazon.py
@@ -794,3 +794,17 @@ def maximalSquare(self, matrix: List[List[str]]) -> int:
                 dp[i][j] = min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) + 1
                 maxSquare = max(maxSquare, dp[i][j])
     return maxSquare ** 2
+
+def breakPalindrome(self, palindrome: str) -> str:
+    p1 = 0
+    p2 = len(palindrome)-1
+    new_pal = list(palindrome)
+    while p1 < p2:
+        if new_pal[p1] != "a":
+            new_pal[p1] = "a"
+            return "".join(new_pal)
+        p1+=1
+        p2-=1
+    if len(new_pal) == 1: return ""
+    new_pal[-1] = "b"
+    return "".join(new_pal)
