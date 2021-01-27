cbe5592638b93e25770b41105dd4d0f0ab75413b
Sukesh Ram <sukeshram7245@gmail.com>
2021-01-26 17:44:26 -0700

    Update Amazon.py

--- Amazon.py
+++ Amazon.py
@@ -743,3 +743,54 @@ def suggestedProducts(self, A, searchWord):
         cur += c
         ans.append(trie.findWord(cur))
     return ans
+
+def search2DMatrix(matrix, target):
+    # an empty matrix obviously does not contain `target` (make this check
+    # because we want to cache `width` for efficiency's sake)
+    if len(matrix) == 0 or len(matrix[0]) == 0: return False
+    # cache these, as they won't change.
+    height, width = len(matrix), len(matrix[0])
+    # start our "pointer" in the bottom-left
+    row, col = height - 1, 0
+    while col < width and row >= 0:
+        if matrix[row][col] > target:
+            row -= 1
+        elif matrix[row][col] < target:
+            col += 1
+        else: # found it
+            return True
+    return False
+
+# Critical Connections
+def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
+    def dfs(rank, curr, prev):
+        low[curr], result = rank, []
+        for neighbor in edges[curr]:
+            if neighbor == prev: continue
+            if not low[neighbor]:
+                result += dfs(rank + 1, neighbor, curr)
+            low[curr] = min(low[curr], low[neighbor])
+            if low[neighbor] >= rank + 1:
+                result.append([curr, neighbor])
+        return result
+
+    low, edges = [0] * n, collections.defaultdict(list)
+    for u, v in connections:
+        edges[u].append(v)
+        edges[v].append(u)
+
+    return dfs(1, 0, -1)
+
+# Maximal Square
+def maximalSquare(self, matrix: List[List[str]]) -> int:
+    rows = len(matrix)
+    cols = len(matrix[0]) if rows > 0 else 0
+    maxSquare = 0
+    dp = [[0] * (cols + 1) for i in range(rows + 1)]
+    
+    for i in range(1, rows + 1):
+        for j in range(1, cols + 1):
+            if (matrix[i - 1][j - 1] == '1'):
+                dp[i][j] = min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) + 1
+                maxSquare = max(maxSquare, dp[i][j])
+    return maxSquare ** 2
